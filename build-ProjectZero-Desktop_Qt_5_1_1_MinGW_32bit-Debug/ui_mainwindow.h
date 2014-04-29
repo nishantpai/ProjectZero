9b1d712f0a281b54710f371bc4e12f49df3a3c5b
darshit12 <darshit12@yahoo.co.in>
2014-04-29 12:32:57 +0530

    Added Button
    Named Darshit

--- build-ProjectZero-Desktop_Qt_5_1_1_MinGW_32bit-Debug/ui_mainwindow.h
+++ build-ProjectZero-Desktop_Qt_5_1_1_MinGW_32bit-Debug/ui_mainwindow.h
@@ -28,6 +28,8 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -42,6 +44,12 @@ public:
         pushButton = new QPushButton(centralWidget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
         pushButton->setGeometry(QRect(60, 30, 75, 23));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(50, 80, 91, 28));
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setGeometry(QRect(70, 140, 75, 23));
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
@@ -63,6 +71,8 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
         pushButton->setText(QApplication::translate("MainWindow", "Raj", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "NIshant", 0));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Darshit", 0));
     } // retranslateUi
 
 };
